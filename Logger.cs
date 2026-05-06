da8a497b0877b8c742c05ca26d78880d8df446fd
BanYeHanFeng <BanYeHanFeng@gmail.com>
2026-05-06 21:18:24 +0000

    perf: 反射检测 BepInEx Debug 等级，框架未开启时跳过 Logger.Debug 减少开销

--- Logger.cs
+++ Logger.cs
@@ -1,8 +1,44 @@
 ﻿using System;
+using System.Collections;
+using System.Reflection;
 using XUnity.Common.Logging;
 
 public static class Logger
 {
+  static bool _debugEnabled = false;
+
+  // 通过反射检测 BepInEx 框架是否开启了 Debug 日志等级
+  public static void AutoDetectDebug()
+  {
+    try
+    {
+      foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+      {
+        if (!asm.GetName().Name.StartsWith("BepInEx")) continue;
+        var loggerType = asm.GetType("BepInEx.Logging.Logger");
+        if (loggerType == null) continue;
+        var listenersProp = loggerType.GetProperty("Listeners", BindingFlags.Public | BindingFlags.Static);
+        if (listenersProp == null) continue;
+        var listeners = listenersProp.GetValue(null) as IEnumerable;
+        if (listeners == null) continue;
+        foreach (var listener in listeners)
+        {
+          var filterProp = listener.GetType().GetProperty("LogLevelFilter");
+          if (filterProp != null)
+          {
+            int filterVal = (int)filterProp.GetValue(listener, null);
+            if ((filterVal & 32) != 0) // BepInEx.Logging.LogLevel.Debug = 32
+            {
+              _debugEnabled = true;
+              return;
+            }
+          }
+        }
+      }
+    }
+    catch { }
+  }
+
   static void Log(string message, string levelTag)
   {
     var logMessage = $"[ALLM_{levelTag}]: [{DateTime.Now:HH:mm:ss}] {message}";
@@ -18,7 +54,7 @@ public static class Logger
   }
 
   public static void Info(string message)  => Log(message, "I");
-  public static void Debug(string message) => Log(message, "D");
+  public static void Debug(string message) { if (_debugEnabled) Log(message, "D"); }
   public static void Warn(string message)  => Log(message, "W");
   public static void Error(string message) => Log(message, "E");
 }
